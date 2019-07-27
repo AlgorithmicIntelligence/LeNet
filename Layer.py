775345f1c0e76c666528ab3f86a042998b9eb72b
NickWang-Windows <seark1219@gmail.com>
2019-07-27 12:42:25 +0800

    MNIST DOWNLOAD & SQUISHING FUNCTION

--- Layer.py
+++ Layer.py
@@ -1,12 +1,11 @@
 import numpy as np
 
 class ConvolutionalLayer():
-    def __init__(self, inputs, size, filters, pad="SAME", stride=1, activation_function=None):
-        assert(inputs.ndim == 4)
-        self.weights = np.random.uniform(-1, 1, [*size, inputs.shape[-1], filters])
-        self.biases = np.random.uniform(-1,1,filters)
-        self.size = size
-        self.filters = filters
+    def __init__(self, weights_shape, pad="SAME", stride=1, activation_function=None):
+        Fi = np.prod(weights_shape[:3])
+        self.shape = weights_shape
+        self.weights = np.random.uniform(-2.4/Fi, 2.4/Fi, weights_shape)
+        self.biases = np.random.uniform(-1, 1, weights_shape[-1])
         self.activation_function = activation_function
         self.pad = pad
         self.stride = stride
@@ -14,33 +13,39 @@ class ConvolutionalLayer():
     def forward_propagation(self, inputs):
         self.inputs = inputs
         if self.pad == "SAME":
-            inputs = np.pad(inputs, (((self.size[0]-1)//2, self.size[0]//2), ((self.size[1]-1)//2, self.size[1]//2)), "constant")
-        # elif self.pad == "VALID":
-        outputs = np.zeros([(inputs.shape[0]-self.size[0])//self.stride + 1, (inputs.shape[1]-self.size[1])//self.stride + 1, self.filters])
+            inputs = np.pad(inputs, (((self.shape[0]-1)//2, self.shape[0]//2), ((self.shape[1]-1)//2, self.shape[1]//2), (0, 0)), "constant")
+
+        outputs = np.zeros([(inputs.shape[0]-self.shape[0])//self.stride + 1, (inputs.shape[1]-self.shape[1])//self.stride + 1, self.shape[-1]])
         for h in range(outputs.shape[0]):
             for w in range(outputs.shape[1]):
                 for f in range(outputs.shape[2]):
-                    outputs[h, w, f] = np.tensordot(inputs[h*self.stride:h*self.stride+self.size[0], w*self.stride:w*self.stride+self.size[1], :] * self.weights[..., f]) + self.bias[f]
+                    outputs[h, w, f] = np.tensordot(inputs[h*self.stride:h*self.stride+self.shape[0], w*self.stride:w*self.stride+self.shape[1], :] * self.weights[..., f]) + self.bias[f]
+
         if self.activation_function == "SIGMOID":
             outputs = 1/(1+np.exp(outputs))
         elif self.activation_function == "SQUASHING":
             outputs = 1.7159 * np.tanh(2/3 * outputs)
+            
         return outputs
 
     def backward_propagation(self, d_outputs, learning_rate):
         if self.activation_function == "SIGMOID":
             d_outputs = (1-d_outputs) * d_outputs
+        elif self.activation_function == "SQUASHING":
+            d_outputs = 1.7159 * 2/3 / np.power(np.cosh(2/3 * d_outputs), 2)
+        
         d_inputs = np.zeros(self.inputs.shape)
         d_weights = np.zeros(self.weights.shape)
         d_biases = np.zeros(self.biases)
         if self.pad == "SAME":
-            inputs = np.pad(self.inputs, (((self.size[0]-1)//2, self.size[0]//2), ((self.size[1]-1)//2, self.size[1]//2),(0, 0)), "constant")
-            d_inputs = np.pad(d_inputs, (((self.size[0]-1)//2, self.size[0]//2), ((self.size[1]-1)//2, self.size[1]//2),(0, 0)), "constant")
+            inputs = np.pad(self.inputs, (((self.shape[0]-1)//2, self.shape[0]//2), ((self.shape[1]-1)//2, self.shape[1]//2),(0, 0)), "constant")
+            d_inputs = np.pad(d_inputs, (((self.shape[0]-1)//2, self.shape[0]//2), ((self.shape[1]-1)//2, self.shape[1]//2),(0, 0)), "constant")
         for h in range(d_outputs.shape[0]):
             for w in range(d_outputs.shape[1]):
-                d_inputs[h*self.stride:h*self.stride+self.size[0], w*self.stride:w*self.stride+self.size[1], :] += d_outputs[h, w, :] * self.weights
-                d_weights += d_outputs[h, w, :] * inputs[h*self.stride:h*self.stride+self.size[0], w*self.stride:w*self.stride+self.size[1], :]
+                d_inputs[h*self.stride:h*self.stride+self.shape[0], w*self.stride:w*self.stride+self.shape[1], :] += d_outputs[h, w, :] * self.weights
+                d_weights += d_outputs[h, w, :] * inputs[h*self.stride:h*self.stride+self.shape[0], w*self.stride:w*self.stride+self.shape[1], :]
                 d_biases += d_outputs[h, w, :]
+                
         if self.pad == "SAME":
             d_inputs = d_inputs[(self.size[0]-1)//2:self.d_inputs.shape[0]-self.size[0]//2+1, (self.size[1]-1)//2:self.d_inputs.shape[1]-self.size[1]//2+1]
         #update
--- Lenet5.py
+++ Lenet5.py
@@ -3,4 +3,4 @@ import Layer
 class Lenet5():
     def __init__(self, inputs):
         self.C1 = Layer.ConvolutionalLayer(inputs,(5, 5), 6, "VALID")
-        self.S2 = Layer.PoolingLayer(sel)
+        self.S2 = Layer.PoolingLayer(self)
